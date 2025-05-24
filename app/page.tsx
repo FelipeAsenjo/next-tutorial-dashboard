4dc3827408727897dc1d7c9aab4f575c07623803
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-24 13:58:34 -0400

    [font] Adding secondary font to specific elements

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
 // import clsx from 'clsx';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
@@ -17,14 +18,15 @@ export default function Page() {
         )}
       ></span> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        {/* the <AcmeLogo /> component also uses Lusitana. It was commented out to prevent errors */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div
             className={ styles.shape }
           />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
