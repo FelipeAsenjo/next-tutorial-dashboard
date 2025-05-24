1d603ab951d3b093a2fcb5718f2215db0cb78532
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-24 13:15:43 -0400

    [style] Conditional styles example with clsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,20 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
+// import clsx from 'clsx';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      {/* <span
+        className={clsx(
+          'inline-flex items-center rounded-full px-2 py-1 text-sm',
+          {
+            'bg-gray-100 text-gray-500': status === 'pending',
+            'bg-green-500 text-white': status === 'paid',
+          },
+        )}
+      ></span> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
